095082609406637e56d285408632ddd4ac1f9b4f
Adrian Hartarto Pramudita <13515091@std.stei.itb.ac.id>
2018-03-11 15:58:32 +0700

    edit dialog box

---
+++ SimulaRoom/Assets/DialogHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogHandler : MonoBehaviour {
+	void Update () 
+	{
+		if(Input.GetMouseButton(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+			{
+				DialogObject obj = hit.collider.GetComponent<DialogObject >();
+				if(obj)
+				{
+					obj.TriggerDialog();
+				}
+			}
+		}
+	}
+}
---
+++ SimulaRoom/Assets/DialogTVBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTVBox : MonoBehaviour {
+	public string[] dialogue = {"This is a Television.\nPress Enter...",
+		"You can watch a movie with this.\nPress Enter...",
+		"Don't throw this Television,\nPress Enter...",
+		"Because it can break easily.\nPress Enter..."};
+	int index = 999;
+	Rect dialogueRect = new Rect(150,220,500,50);
+
+
+	void Update() {
+		if (Input.GetKeyDown("return")) 
+		{
+			index++;
+		} 
+	}
+
+	void OnGUI() {
+		if((Event.current.Equals(Event.KeyboardEvent ("E")))) {
+			index = 0;
+			GUI.Box(dialogueRect, dialogue[index]);
+		}
+		if (index < dialogue.Length) {
+			GUI.Box(dialogueRect, dialogue[index]);
+		} else {
+		}
+	}
+}
